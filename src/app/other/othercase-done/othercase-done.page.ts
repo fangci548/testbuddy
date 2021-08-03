c8dde63182ecedc920efc521eac8ba291bad55fa
Chao <you@example.com>
2021-08-04 01:16:20 +0800

    0804

--- src/app/other/othercase-done/othercase-done.page.ts
+++ src/app/other/othercase-done/othercase-done.page.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { Case } from '../../models/case.model';
+import { CaseService } from './../../service/case.service';
 
 @Component({
   selector: 'app-othercase-done',
   templateUrl: './othercase-done.page.html',
   styleUrls: ['./othercase-done.page.scss'],
 })
+
 export class OthercaseDonePage implements OnInit {
 
-  constructor() { }
+
+  cases: Case[] = [];
+
+  constructor(
+    private caseService: CaseService) { }
 
   ngOnInit() {
+    this.cases = this.caseService.getCases();
   }
 
 }
