19349313a120074afbf191d479223f50464506e1
Chao <you@example.com>
2021-08-09 20:23:27 +0800

    0809#

--- src/app/setting/coin-manage/coin-manage.page.ts
+++ src/app/setting/coin-manage/coin-manage.page.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { NavController } from '@ionic/angular';
 
+
 @Component({
   selector: 'app-coin-manage',
   templateUrl: './coin-manage.page.html',
